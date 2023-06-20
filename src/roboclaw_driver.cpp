af04d6b2cd5f9cccf49fc59fda3a37abcfe52b2a
Manuel Escobar <manescob@fiu.edu>
2023-06-20 14:59:32 -0400

    tested lib

---
+++ src/roboclaw_driver.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "mcu_serial_com/Serial.h"
+
+class Reinterpretor {
+public:
+    void operator()(const std::string &message) const {
+        std::cout << "arduino reinterpreted>" << message << std::endl;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Serial serial("/dev/ttyACM0");
+    std::string cmd;
+    while (true) {
+        std::cout << "?> ";
+        std::getline(std::cin, cmd);
+        if (cmd == "bye")
+            break;
+        serial.write(cmd);
+    }
+    std::cout << "Bye!" << std::endl;
+    return 0;
+}
+
